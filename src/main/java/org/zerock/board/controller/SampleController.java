9635b1350958a6bf5c716dfb2b7315ae82f5d9e1
10-5\kkw <java960318@naver.com>
2024-04-08 16:25:50 +0900

    JPA, 페이징, Sort

---
+++ src/main/java/org/zerock/board/controller/SampleController.java
@@ -0,0 +1,15 @@
+package org.zerock.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //기본값 json으로 처리한다.
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String[] hello(){
+        return new String[] {"Hello", "World"};
+    }
+
+
+}
